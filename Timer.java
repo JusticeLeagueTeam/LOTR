a5254c56d0120063834edf2219b36a7e35c7e918
olihorvath <horvatholiver.g@gmail.com>
2014-04-12 12:25:07 +0200

    commit oliver
    Timer osztály változtatása

--- Timer.java
+++ Timer.java
@@ -13,15 +13,19 @@ public class Timer {
 		System.out.println("Timer run - ");
 	}
 
+	/** Beállítja a periódusidőt */
 	public void setPeriod(int intParameter) {
 		this.period = intParameter;
 		System.out.println("Timer setPeriod - periodusio beallitasa");
 	}
 
+	/** Az órajelért felelős metódus, minden periódusidő lejártakor meghívódik. 
+	 * Meghívja a Map és a Game tick metódusát.
+	 */
 	public static void tick() {
+		Map.tick();
+		Game.tick();
 		System.out.println("Timer tick - letelt egy orajel");
 	}
 	
-
-
 }
