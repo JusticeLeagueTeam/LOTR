e8cd66b2b86a0582a4a3dc8de294f4669267c03e
olihorvath <horvatholiver.g@gmail.com>
2014-04-16 12:12:53 +0200

    Update Position.java

--- Position.java
+++ Position.java
@@ -15,48 +15,50 @@ public class Position {
 	 * cellamatrixon beluli oszlopszam 
 	 */
 	private int columnValue;
-	
+	/**
+	 * utat jelzo flag
+	 */
 	private boolean roadFlag;
 
 	/**
 	 *  visszaadja a sorszamot 
 	 */
 	public int getRowValue() {
-		System.out.println("Position getRowValue() - Sorszam lekerdezese.");
 		return this.rowValue;
 	}
 
 	/** 
 	 * beallitja a sorszamot
 	 */
-	public void setRowValue(int rowValue) {
-		System.out.println("Position setRowValue(int rowValue) - Sorszam beallitasa.");
-		this.rowValue = rowValue;
+	public void setRowValue(int rV) {
+		this.rowValue = rV;
 	}
 
 	/**
 	 *  visszaadja az oszlopszamot
 	 */
 	public int getColumnValue() {
-		System.out.println("Position setRowValue() - Oszlopszam lekerdezese.");
 		return this.columnValue;
 	}
 
 	/** 
-	 * visszaadja az oszlopszamot 
+	 * beallitja az oszlopszamot 
 	 */
-	public void setColumnValue(int columnValue) {
-		System.out.println("Position setColumnValue(int columnValue) - Oszlopszam beallitasa.");
-		this.columnValue = columnValue;
+	public void setColumnValue(int cV) {
+		this.columnValue = cV;
 	}
-	
+	/**
+	 * utat jelzo flag-et adja vissza
+	 * @return roadFlag
+	 */
 	public boolean getRoadFlag() {
 		return this.roadFlag;
 	}
-	
-	public void setRoadFlag(boolean roadflag) {
-		this.roadFlag = roadflag;
+	/**
+	 * utat jelzo flag-et allitja be
+	 * @param rF
+	 */
+	public void setRoadFlag(boolean rF) {
+		this.roadFlag = rF;
 	}
-
-
 }
